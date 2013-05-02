c8478ca8106ab9e7617cba0c032f98a2eeba46ef
John Bowman <acs.bowman@gmail.com>
2013-05-02 12:17:33 -0400

    Added several GIT tools

---
+++ Conference/Tools/set_npp_commit.cmd
@@ -0,0 +1,2 @@
+rem git config --global core.editor "'C:\Program Files (x86)\Notepad++\notepad++.exe' -multiInst -notabbar -nosession -noPlugin"
+git config --global core.editor "'C:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin"
---
+++ Conference/Tools/set_vim_commit.cmd
@@ -0,0 +1,1 @@
+git config core.editor 'vim'
